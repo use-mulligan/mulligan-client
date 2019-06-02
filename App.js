78987f16f8b1cd9b3f73813ae5e7e428c4a4ab84
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-06-02 19:53:51 -0400

    added import for Apollo Client

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import ApolloClient from "apollo-boost";
 import { ApolloProvider } from 'react-apollo';
+import client from './data/services/client'
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/"
-});
 // Components
 import Login from './components/Login'
 
@@ -13,7 +10,7 @@ export default class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client} style={styles.container}>
-        <Login username="" password=""/>
+        <Login />
         <View></View>
       </ApolloProvider>
     );
