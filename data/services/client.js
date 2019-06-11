dbb11c99a377748b436096219a0929132ec2cf72
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-06-10 20:47:44 -0400

    updated code for apollo-booost@0.4.1

--- data/services/client.js
+++ data/services/client.js
@@ -1,12 +1,7 @@
 import ApolloClient from "apollo-boost";
-/**
- * NOTE: If using real device through USB for development
- * set 'uri' to the IPV4 address of your server/localhost (your computer)
- * to allow network calls within your private network.
- */
+
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
-  // credentials: 'same-origin',
+  uri: "http://localhost:4000/"
 });
 
 export default client;
