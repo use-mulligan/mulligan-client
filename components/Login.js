2ea09b98a0d93616ba2ff65a5faec87df5e6d46e
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-06-22 19:51:07 -0400

    fixed react-navigation props

--- components/Login.js
+++ components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Text,
   TextInput,
@@ -6,133 +6,138 @@ import {
   TouchableOpacity,
   StyleSheet,
   KeyboardAvoidingView
-} from 'react-native'
-import { ApolloProvider } from 'react-apollo';
-import client from '../data/services/client'
-import UserLoginMutation from '../data/mutations/login'
+} from "react-native";
+import { ApolloProvider } from "react-apollo";
+import client from "../data/services/client";
+import UserLoginMutation from "../data/mutations/login";
 
 export default class LoginScreen extends React.Component {
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <ApolloProvider client={client} style={loginScreenStyles.container}>
-        <Login navigate={navigate}/>
-        <View></View>
+        <Login navigation={navigate} />
+        <View />
       </ApolloProvider>
-    )
+    );
   }
 }
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: "Hey",
-      password: "Yo"
-    }
   }
 
-  submitCreds =  async (emailEntered, passwordEntered) => {
-    client.mutate({
-      variables: { email: emailEntered, password: passwordEntered },
-      mutation: UserLoginMutation,
-    }).then(result => {
-      console.log(JSON.stringify(result))
-      this.props.navigate('Home')
-    })
-    .catch(error => { console.log(error) });
-  }
+  submitCreds = async (emailEntered, passwordEntered) => {
+    client
+      .mutate({
+        variables: { email: emailEntered, password: passwordEntered },
+        mutation: UserLoginMutation
+      })
+      .then(result => {
+        console.log(JSON.stringify(result));
+        this.props.navigation("Home");
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   render() {
-    const { email, password } = this.state
     return (
       <KeyboardAvoidingView behavior="padding" style={loginStyles.container}>
         <View style={loginStyles.loginContainer}>
           <Text style={loginStyles.loginTitle}>Mulligan</Text>
-          <Text>{email} {password}</Text>
         </View>
         <View style={loginStyles.formContainer}>
-          <LoginForm getCreds={(e, p) => this.submitCreds(e, p)}/>
+          <LoginForm getCreds={(e, p) => this.submitCreds(e, p)} />
         </View>
       </KeyboardAvoidingView>
     );
   }
 }
 
-class LoginForm extends React.Component  {
+class LoginForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   handleSubmit = () => {
     this.props.getCreds(this.email, this.password);
-  }
+  };
 
   render() {
     return (
       <>
-        <TextInput style = {loginStyles.input}
+        <TextInput
+          style={loginStyles.input}
           autoCapitalize="none"
           onSubmitEditing={() => this.passwordInput.focus()}
           autoCorrect={false}
-          keyboardType='email-address'
+          keyboardType="email-address"
           returnKeyType="next"
-          onChangeText={(input)=> this.email = input}
-          placeholder='Email or Mobile Num'
-          placeholderTextColor='rgba(225,225,225,0.7)'/>
+          onChangeText={input => (this.email = input)}
+          placeholder="Email or Mobile Num"
+          placeholderTextColor="rgba(225,225,225,0.7)"
+        />
 
-        <TextInput style = {loginStyles.input}
+        <TextInput
+          style={loginStyles.input}
           returnKeyType="go"
-          onChangeText={(input)=> this.password = input}
-          placeholder='Password'
-          placeholderTextColor='rgba(225,225,225,0.7)'
-          secureTextEntry />
+          onChangeText={input => (this.password = input)}
+          placeholder="Password"
+          placeholderTextColor="rgba(225,225,225,0.7)"
+          secureTextEntry
+        />
 
-        <TouchableOpacity style={loginStyles.buttonContainer} onPress={this.handleSubmit}>
+        <TouchableOpacity
+          style={loginStyles.buttonContainer}
+          onPress={this.handleSubmit}
+        >
           <Text style={loginStyles.buttonText}>LOGIN</Text>
         </TouchableOpacity>
       </>
-    )
-
+    );
   }
 }
 
 const loginStyles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: '#2c3e50',
+    flex: 1,
+    backgroundColor: "#2c3e50"
   },
-  loginContainer:{
-      alignItems: 'center',
-      flexGrow: 1,
-      justifyContent: 'center'
+  loginContainer: {
+    alignItems: "center",
+    flexGrow: 1,
+    justifyContent: "center"
   },
   loginTitle: {
-      fontSize: 40,
-      color: '#fff',
-      textAlign: 'center',
-      fontWeight: '700'
+    fontSize: 40,
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700"
   },
-  input:{
+  input: {
     height: 40,
-    backgroundColor: 'rgba(225,225,225,0.2)',
+    backgroundColor: "rgba(225,225,225,0.2)",
     marginBottom: 10,
     padding: 10,
-    color: '#fff'
+    color: "#fff"
   },
-  buttonContainer:{
-      backgroundColor: '#2980b6',
-      paddingVertical: 15
+  buttonContainer: {
+    backgroundColor: "#2980b6",
+    paddingVertical: 15
   },
-  buttonText:{
-      color: '#fff',
-      textAlign: 'center',
-      fontWeight: '700'
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700"
   }
 });
 
 const loginScreenStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
