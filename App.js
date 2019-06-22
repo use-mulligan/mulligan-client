5c165157b9b3724fa136524b40596220b2a22b53
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-06-22 19:53:08 -0400

    reworked react-navigation steup with initial route and fixed the AppContainer set up

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { createStackNavigator, createAppContainer} from 'react-navigation'
-import LoginScreen from './components/Login';
-import HomeScreen from './components/Home';
+import React from "react";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import LoginScreen from "./components/Login";
+import HomeScreen from "./components/Home";
 
-const MainNavigator = createStackNavigator({
-  Login: {screen: LoginScreen},
-  Home: {screen: HomeScreen}
-})
+const MainNavigator = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Home: HomeScreen
+  },
+  {
+    initialRouteName: "Home"
+  }
+);
 
-const App = createAppContainer(MainNavigator);
+const AppContainer = createAppContainer(MainNavigator);
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
