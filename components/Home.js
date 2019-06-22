498be125ceff49450bef0c14add782485df09f7a
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-06-22 19:48:34 -0400

    added Home/Main page screen with buttons :D

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,70 @@
-import React from 'react';
-import {
-  View,
-} from 'react-native'
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <View>
-        Hey you're logged in.
-      </View>
-    )
+      <Home>
+        <StartGameButton />
+        <MyScoreCardsButton />
+      </Home>
+    );
   }
 }
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <View style={homeStyles.container}>{this.props.children}</View>;
+  }
+}
+
+class StartGameButton extends React.Component {
+  startNewGame = () => {};
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={homeStyles.buttonContainer}
+        onPress={this.startNewGame}
+      >
+        <Text style={homeStyles.buttonText}>START GAME</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class MyScoreCardsButton extends React.Component {
+  getMyScoreCards = () => {};
+
+  render() {
+    return (
+      <TouchableOpacity
+        style={homeStyles.buttonContainer}
+        onPress={this.getMyScoreCards}
+      >
+        <Text style={homeStyles.buttonText}>MY SCORECARDS</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const homeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#2c3e50"
+  },
+  buttonContainer: {
+    backgroundColor: "#2980b6",
+    paddingVertical: 15,
+    marginVertical: 15
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+    fontWeight: "700"
+  }
+});
