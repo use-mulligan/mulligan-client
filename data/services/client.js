b5158b71a255450a5c4a85309e2d9f2e700cd76b
Austin Howlett   url = git@bitbucket.org:unraveldevops/unravel-profile-service.git <austinhowletttech@gmail.com>
2019-07-07 13:26:59 -0400

    migrated from apollo-boost to apollo-client, implemented token headers, cache storage, subscription functionality, network status query

--- data/services/client.js
+++ data/services/client.js
@@ -1,7 +1,85 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { withClientState } from "apollo-link-state";
+import { ApolloLink, Observable } from "apollo-link";
+import { AsyncStorage } from "react-native";
 
+const cache = new InMemoryCache({
+  cacheRedirects: {
+    Query: {
+      // Cache something like user data or scorecards
+    }
+  }
+});
+
+const request = async operation => {
+  const access_token = await AsyncStorage.getItem("@mulligan:access_token");
+  if (access_token) {
+    operation.setContext({
+      header: {
+        authorization: access_token
+      }
+    });
+  }
+};
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable(observer => {
+      let handle;
+      Promise.resolve(operation)
+        .then(oper => request(operation))
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer)
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
 const client = new ApolloClient({
-  uri: "http://138.197.128.14/"
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          )
+        );
+      }
+      if (networkError) {
+        // logout user
+      }
+    }),
+    requestLink,
+    withClientState({
+      defaults: {
+        isConnected: true
+      },
+      resolvers: {
+        Mutation: {
+          updateNetworkStatus: (_, { isConnected }, { cache }) => {
+            cache.writeData({ data: { isConnected } });
+            return null;
+          }
+        }
+      },
+      cache
+    }),
+    new HttpLink({
+      uri: "http://138.197.128.14/",
+      credentials: "same-origin"
+    })
+  ]),
+  cache
 });
 
 export default client;
